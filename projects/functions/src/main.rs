4cbb30269b728778e01cdc6e7ee15e0d35b95252
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2023-03-07 17:25:46 -0300

    add five function

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(6);
+    another_function(6, 'm');
+
+    let x = five();
+
+    println!("The value of x is: {x}");
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn another_function(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
 }
