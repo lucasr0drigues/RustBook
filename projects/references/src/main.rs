9e84d7ef0501797f1b775561c2a86785bde8fe3e
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2023-03-12 01:12:21 -0300

    add change fn

--- projects/references/src/main.rs
+++ projects/references/src/main.rs
@@ -4,8 +4,19 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
+
+    let mut s2 = s1;
+
+    change(&mut s2);
+
+    println!("the value of s2 is {s2}");
 }
 
-fn calculate_length(s: &String) -> usize {
+fn calculate_length(s: &String) -> usize { // s is a reference to a String
     s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what it refers to, it is not dropped.
+
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world!");
 }
